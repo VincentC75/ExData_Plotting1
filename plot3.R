d295025f5abdaac2a8eca27e5188835ee1123e67
VincentC75 <vicourtois@orange.fr>
2018-09-12 17:12:06 +0200

    Commit plot 2, 3 and 4

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-# Plot1.R
+# Plot3.R
 
 # Load the data
 power <- read.csv2('household_power_consumption.txt', stringsAsFactors = FALSE)
@@ -19,6 +19,6 @@ pow$Sub_metering_2 <- as.numeric(pow$Sub_metering_2)
 pow$Sub_metering_3 <- as.numeric(pow$Sub_metering_3)
 
 # Display first the plot on screen, and then save to file
-hist(pow$Global_active_power, xlab="Global Active Power (kilowatts)", main = "Global Active Power", col = "Red")
-dev.copy(png,'plot1.png')
+plot(pow$Date, pow$Global_active_power, ylab="Global Active Power(kilowatts)", xlab = "", type = "l")
+dev.copy(png,'plot3.png')
 dev.off()
---
+++ plot4.R
@@ -0,0 +1,42 @@
+# Plot3.R
+
+# Load the data
+power <- read.csv2('household_power_consumption.txt', stringsAsFactors = FALSE)
+
+# Filter the required days
+pow <- power[power$Date == "1/2/2007" | power$Date == "2/2/2007",]
+
+# Convert the data to appropriate format
+library(lubridate)
+pow$Date <- dmy_hms( paste(pow$Date , pow$Time) )
+pow$Time <- NULL
+pow$Global_active_power <- as.numeric(pow$Global_active_power)
+pow$Global_reactive_power <- as.numeric(pow$Global_reactive_power)
+pow$Voltage <- as.numeric(pow$Voltage)
+pow$Global_intensity <- as.numeric(pow$Global_intensity)
+pow$Sub_metering_1 <- as.numeric(pow$Sub_metering_1)
+pow$Sub_metering_2 <- as.numeric(pow$Sub_metering_2)
+pow$Sub_metering_3 <- as.numeric(pow$Sub_metering_3)
+
+# Display first the plot on screen, and save to file
+# 2 rows, 2 columns for rows
+par(mfrow=c(2,2))
+
+# plot1
+plot(pow$Date, pow$Global_active_power, ylab="Global Active Power(kilowatts)", xlab = "", type = "l")
+
+# plot2
+plot(pow$Date, pow$Voltage, ylab="Volatge", xlab = "datetime", type = "l")
+
+# plot3
+plot(pow$Date, pow$Sub_metering_1, ylab="Energy sub metering", xlab = "", type = "l")
+lines(pow$Date, pow$Sub_metering_2, col="red")
+lines(pow$Date, pow$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), cex= 0.8, lty = 1)
+
+#plot4
+plot(pow$Date, pow$Global_reactive_power, ylab="Global_reactive_power", xlab = "datetime", type = "l")
+
+# Save to file
+dev.copy(png,'plot4.png')
+dev.off()
